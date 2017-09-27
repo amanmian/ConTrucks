be5cd36f77bbe5861eb2e004b11daf98be6a1368
amanmian <amanmian.ksp@gmail.com>
2017-09-27 15:29:56 +0530

    ConTrucks

---
+++ Contrucks.Service/Interfaces/IRecentJobPostService.cs
@@ -0,0 +1,18 @@
+﻿using Contrucks.model;
+using Contrucks.model.ViewModels;
+using System.Collections.Generic;
+
+namespace Contrucks.Service.Interfaces
+{
+    public interface IRecentJobPostService
+    {
+        IEnumerable<NewJobPosts> GetAll();
+        void AddData(RecentpostViewmodel usertables);
+        NewJobPosts GetAllById(int Id);
+        IEnumerable<LoadTypes> GetLoadType();
+        IEnumerable<TruckTypes> GetTruckType();
+        NewJobPosts GetAllById(int Id);
+
+        IEnumerable<NewJobPosts> GetFulfilledPostsData();
+    }
+}
